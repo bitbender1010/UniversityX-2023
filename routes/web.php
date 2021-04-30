42d690663411d8fbc27239feb285ae23c023281d
Dammie <bitbender1010@gmail.com>
2021-04-30 05:28:15 +0100

    v1 update heroku

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/computer-science/100/dashboard', function () {
+    return view('computer-science/100/dashboard');
+});
+
+Route::get('/computer-science/100/CSC120/coursecontent', function () {
+    return view('computer-science/100/CSC120/coursecontent');
+});
+
+//course details
+Route::get('/computer-science/100/CSC120/1/coursedetails', function () {
+    return view('computer-science/100/CSC120/1/coursedetails');
+});
+
+//video routes
+Route::get('/computer-science/100/CSC120/1/video-one', function () {
+    return view('computer-science/100/CSC120/1/video-one');
+});
+
+
