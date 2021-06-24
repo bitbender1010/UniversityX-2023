d03629ef634e55a52d0905fa49cc2c549899ca44
Dammie <bitbender1010@gmail.com>
2021-06-24 14:13:16 +0100

    added for csc

--- resources/views/layouts/student100.blade.php
+++ resources/views/layouts/student100.blade.php
@@ -10,6 +10,16 @@
     <!-- <title>{{ config('app.name', 'UniversityX') }}</title> -->
     <title>UniversityX</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VK6ZVYNXE2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VK6ZVYNXE2');
+    </script>
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/doubletap.js') }}" defer></script>
